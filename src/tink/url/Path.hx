166c7864aced42d429edbcf8878d373e3969c95a
Clément Charmet <clement@secretmedia.com>
2016-11-29 10:41:37 +0100

    allow empty path parts

--- src/tink/url/Path.hx
+++ src/tink/url/Path.hx
@@ -4,23 +4,23 @@ using haxe.io.Path;
 using StringTools;
 
 abstract Path(String) to String {
-  
+
   public function parts()
-    return [for (p in this.split('/')) if (p != '') p];//TODO: consider using a cache
-  
+    return [for (p in this.split('/')) /*if (p != '')*/ p];//TODO: consider using a cache
+
   public var absolute(get, never):Bool;
     inline function get_absolute()
       return this.charAt(0) == '/';
-      
+
   public var isDir(get, never):Bool;
     inline function get_isDir()
       return this.charAt(this.length - 1) == '/';
-    
-  inline function new(s) 
+
+  inline function new(s)
     this = s;
-    
+
   public function join(that:Path):Path
-    return 
+    return
       if (that == '') cast this;
       else if (that.absolute) that;
       else //it's a little heavy to run through the WHOLE normalization here, but eh ... pull requests welcome
@@ -29,43 +29,34 @@ abstract Path(String) to String {
           case -1: that;
           case v: this.substr(0, v + 1) + that;
         }
-  
+
   @:from static public function ofString(s:String) {//TODO: consider what to do with invalid paths, e.g. '....'
     return new Path(normalize(s));
   }
-  
+
   static public function normalize(s:String) {
     s = s.replace('\\', '/').trim();
     if (s == '.')
       return './';
-      
-    var isDir = s.endsWith('/..') || s.endsWith('/') || s.endsWith('/.');
-    
+
     var parts = [],
         isAbsolute = s.startsWith('/'),
         up = 0;
-    
+
     for (part in s.split('/'))
       switch part.trim() {
-        case '':
         case '.':
-        case '..': 
+        case '..':
           if (parts.pop() == null) up++;
         case v: parts.push(v);
       }
-      
-    if (isAbsolute)
-      parts.unshift('');
-    else
+
+    if (!isAbsolute)
       for (i in 0...up)
         parts.unshift('..');
-    
-    if (isDir)
-      parts.push('');
-      
-    
+
     return parts.join('/');
   }
-  
+
   static public var root(default, null):Path = new Path('/');
 }
